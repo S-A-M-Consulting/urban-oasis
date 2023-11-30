21123b31d1ea55b147ac0a450833e9b8df89ee0d
mxtchjohnston <mitchjohnston99@gmail.com>
2023-11-29 20:14:32 -0800

    added park seed data to database

--- server/resetdb.js
+++ server/resetdb.js
@@ -2,6 +2,44 @@ require('dotenv').config();
 
 const fs = require('fs');
 const db = require('./database/connection');
+const axios = require('axios');
+
+const readJsonFile = (filename) => {
+  try {
+    // Read the content of the JSON file synchronously
+    const data = fs.readFileSync(filename, 'utf8');
+  
+    // Parse the JSON data
+    const jsonData = JSON.parse(data);
+  
+    // Now jsonData contains the parsed JSON object
+    //console.log(jsonData);
+    return jsonData;
+  } catch (error) {
+    console.error(`Error reading or parsing JSON: ${error}`);
+  }
+}
+
+const seedParkData = async (data) => {
+  console.log("starting seed parkData", data.length);
+  
+  for (const park of data) {
+    const {name, geometry, vicinity, photos, rating} = park;
+    const {lat, lng} = geometry.location;
+    //const photo = photos[0].photo_reference || " ";
+
+    const parkData = {
+      name,
+      latitude: lat,
+      longitude: lng,
+      streetAddress: vicinity,
+      googleRating: rating
+    };
+
+    await axios.post('http://localhost:8080/api/park', parkData);
+    console.log('parkData:', parkData);
+  }
+}
 
 const runSchemaFiles = async () => {
   console.log('Starting to build tables...');
@@ -14,8 +52,19 @@ const runSchemaFiles = async () => {
 }
 
 const runSeedFiles = async () => {
-  console.log(`-> Loading Seeds ...`);
-  await axios.post("http://localhost:8080/api/park", {})
+  console.log('Starting to seed tables...');
+  const schemaFiles = fs.readdirSync('./database/seeds');
+  for (const fn of schemaFiles) {
+    const sql = fs.readFileSync(`./database/seeds/${fn}`, "utf8");
+    console.log(`\t-> Running ${fn}`);
+    await db.query(sql);
+  }
+  console.log('Done seeding tables, reading JSON file...');
+  const data = readJsonFile('./all_results.json');
+  //console.log(data[0].results);
+  await seedParkData(data[0].results);
+  console.log('Done reading JSON file.');
+
 };
 
 const runResetDb = async () => {
