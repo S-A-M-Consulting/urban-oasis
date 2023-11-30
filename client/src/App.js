0e4bc755d6b9a2f65264ba5b45d2a0015024be22
stephen-fraser <steve.james.fraser@gmail.com>
2023-11-29 21:28:10 -0800

    install clusters and added layers for alternate map views

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+  Popup,
+  LayersControl,
+} from "react-leaflet";
 import "./App.css";
 import "leaflet/dist/leaflet.css";
 import { Icon } from "leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
+const { BaseLayer } = LayersControl;
 
 function App() {
   const parkMarkers = [
@@ -22,18 +30,44 @@ function App() {
 
   return (
     <MapContainer center={[49.02388, -122.801178]} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      <LayersControl position="topright">
+        <BaseLayer checked name="Open Street Map">
+          {/* Default Leaflet Tiles */}
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </BaseLayer>
+        <BaseLayer name="Satellite">
+          {/* GOOGLE MAPS TILES */}
+          <TileLayer
+            attribution="Google Maps"
+            // url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
+            url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
+            // url="http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}" // terrain
+            maxZoom={20}
+            subdomains={["mt0", "mt1", "mt2", "mt3"]}
+          />
+        </BaseLayer>
+        <BaseLayer name="WaterColour">
+          {/* WaterColour Leaflet Tiles */}
+          <TileLayer
+            attribution="Stamen Watercolor"
+            url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg"
+          />
+        </BaseLayer>
 
-      {parkMarkers.map((marker) => (
-        <Marker position={marker.geocode} icon={customIcon}>
-          <Popup>
-            <h2>{marker.popup}</h2>
-          </Popup>
-        </Marker>
-      ))}
+        <MarkerClusterGroup chunkedLoading>
+          {parkMarkers.map((marker) => (
+            // Need to add a key!
+            <Marker position={marker.geocode} icon={customIcon}>
+              <Popup>
+                <h2>{marker.popup}</h2>
+              </Popup>
+            </Marker>
+          ))}
+        </MarkerClusterGroup>
+      </LayersControl>
     </MapContainer>
   );
 }
